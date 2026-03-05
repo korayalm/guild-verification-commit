d343715d5edfdfbf3fd949f524595eeafa9efd41
korayalm <126489308+korayalm@users.noreply.github.com>
2026-03-05 15:12:10 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:12:07.814Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T12:12:14.907Z
+Commit: Introduce real-time notifications
