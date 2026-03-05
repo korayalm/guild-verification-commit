e530758ef7c3bcac5fb07172dff531c88bc6046e
korayalm <126489308+korayalm@users.noreply.github.com>
2026-03-05 15:12:44 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:12:43.731Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T12:12:49.515Z
+Commit: Fix memory leak in event handlers
