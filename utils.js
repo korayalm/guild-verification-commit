5de40beb338cc10b332a85add71e3974ded60e81
korayalm <126489308+korayalm@users.noreply.github.com>
2026-03-05 15:13:50 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:13:33.539Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T12:13:54.643Z
+Commit: Resolve null pointer exception in user service
